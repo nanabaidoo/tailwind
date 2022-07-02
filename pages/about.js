1615551086f7baea8fc0c0bd821587ee96a7aef4
Baidoo Nana Lartey <baidoolartey@gmail.com>
2022-07-02 15:29:20 +0000

    nana

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+
+
+
+function About() {
+  return (
+    <>
+      <div> this is the about page of the Dapp</div>
+      <div>the boy id going to the market</div>
+    </>
+  );
+}
+
+export default About;
---
+++ pages/contact.js
@@ -0,0 +1,11 @@
+import React from 'react' 
+
+const caa =require('react')
+
+function contact() {
+  return (
+    <div>contact</div>
+  )
+}
+
+export default contact
